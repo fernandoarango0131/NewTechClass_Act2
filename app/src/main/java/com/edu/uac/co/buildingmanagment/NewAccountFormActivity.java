9fb4c708c387d7f14d1cee11f092475366ded0ec
your usernametatadavila <tata.davila.a@gmail.com>
2020-03-29 16:05:15 -0500

    Created new activity for the new account form

---
+++ app/src/main/java/com/edu/uac/co/buildingmanagment/NewAccountFormActivity.java
@@ -0,0 +1,14 @@
+package com.edu.uac.co.buildingmanagment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAccountFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account_form);
+    }
+}
